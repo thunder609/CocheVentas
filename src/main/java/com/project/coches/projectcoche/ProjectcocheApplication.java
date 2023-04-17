422dfa2742002e2474cbc1d6e319b204f83a31ce
thunder609 <oswaldo-pachco@hotmail.es>
2023-04-16 20:48:38 -0400

    start project

---
+++ src/main/java/com/project/coches/projectcoche/ProjectcocheApplication.java
@@ -0,0 +1,13 @@
+package com.project.coches.projectcoche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectcocheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectcocheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/coches/projectcoche/ProjectcocheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.coches.projectcoche;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectcocheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
